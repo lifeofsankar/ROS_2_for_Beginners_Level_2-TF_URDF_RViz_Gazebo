61813a7313a8727a5d9162614154bfb6b11f037b
lifeofsankar <jaisankarjaikishan369@gmail.com>
2025-08-28 10:19:15 +0530

    launch file added

--- build/my_robot_description/cmake_install.cmake
+++ build/my_robot_description/cmake_install.cmake
@@ -43,7 +43,10 @@ if(NOT DEFINED CMAKE_OBJDUMP)
 endif()
 
 if(CMAKE_INSTALL_COMPONENT STREQUAL "Unspecified" OR NOT CMAKE_INSTALL_COMPONENT)
-  file(INSTALL DESTINATION "${CMAKE_INSTALL_PREFIX}/share/my_robot_description/" TYPE DIRECTORY FILES "/home/admin/uws/src/my_robot_description/urdf")
+  file(INSTALL DESTINATION "${CMAKE_INSTALL_PREFIX}/share/my_robot_description/" TYPE DIRECTORY FILES
+    "/home/admin/uws/src/my_robot_description/urdf"
+    "/home/admin/uws/src/my_robot_description/launch"
+    )
 endif()
 
 if(CMAKE_INSTALL_COMPONENT STREQUAL "Unspecified" OR NOT CMAKE_INSTALL_COMPONENT)
